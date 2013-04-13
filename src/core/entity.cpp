3dcce90aa449d1e7cf80a9c12eedf73afe683384
kristinalpink <kristinalpink@gmail.com>
2013-04-12 19:03:24 -0700

    added basic physics movement

--- src/core/entity.cpp
+++ src/core/entity.cpp
@@ -2,16 +2,95 @@
  * entity.cpp
  */
 
-Entity::id_gen = 0;
+#include "entity.hpp"
+#include <stdio.h>
+
+int Entity::id_gen = 0;
 
 Entity::Entity() {
-	id = id_gen++;
-	position = new Float3();
-	orientation = new Quaternion();
+	init();
 }
 
-Entity::Entity(Float3 position, Quarternion orientation) {
+Entity::Entity(D3DXVECTOR3 pos, D3DXVECTOR3 dir) {
+	init();
+	this->pos = pos;
+	this->dir = dir;
+}
+
+void Entity::init() {
 	id = id_gen++;
-	this->position = new Float3();
-	this->orientation = new Quaternion();
+	type = ENTITY;
+	pos = D3DXVECTOR3(0.0, 0.0, 0.0);
+	dir = D3DXVECTOR3(0,0,1);
+	force = 100;
+	mass = 100;
+	velocity = 0;
+	maxVelocity = 100;
+	isThrustOn = false;
+}
+
+void Entity::rotate(D3DXVECTOR3 rotation){
+	dir = dir + rotation;
+
+	//D3DXVec3Normalize(&dir,&dir); - linker error
+	float tmp = abs(sqrt((dir.x * dir.x) + (dir.y * dir.y) + (dir.z * dir.z)));
+	dir = D3DXVECTOR3(dir.x/tmp,dir.y/tmp,dir.z/tmp);
+}
+void Entity::toggleThrust() {
+	isThrustOn = !isThrustOn;
+}
+
+void Entity::calculate(float dt){
+	// Calculate applied force
+	float tF =  0;
+	if(isThrustOn) tF = force;
+	else if(velocity != 0) tF = -force;
+
+	float dv = ((tF / mass ) * dt);
+	if(velocity + dv <= maxVelocity)
+		velocity = velocity + dv;
+	pos = pos + velocity*dir;
+}
+
+void main()
+{
+
+	//Testing
+	Entity a = Entity();
+	a.calculate(50);
+
+	printf("%f, %f, %f\n", a.pos.x, a.pos.y, a.pos.z);
+	a.toggleThrust();
+	a.calculate(5);
+	printf("%f, %f, %f\n", a.pos.x, a.pos.y,a.pos.z);
+
+	for(int i = 0; i < 20; i++){
+		a.calculate(5);
+		printf("%f, %f, %f\n", a.pos.x, a.pos.y,a.pos.z);
+	}
+
+	a.toggleThrust();
+
+	for(int i = 0; i < 20; i++){
+		a.calculate(5);
+		printf("%f, %f, %f\n", a.pos.x, a.pos.y,a.pos.z);
+	}
+
+	a.rotate(D3DXVECTOR3(0,0,-2));
+	a.toggleThrust();
+	for(int i = 0; i < 40; i++){
+		a.calculate(5);
+		printf("%f, %f, %f\n", a.pos.x, a.pos.y,a.pos.z);
+	}
+
+	a.rotate(D3DXVECTOR3(0,1,0));
+	for(int i = 0; i < 20; i++){
+		a.calculate(5);
+		printf("%f, %f, %f\n", a.pos.x, a.pos.y,a.pos.z);
+	}
+
+	while(1){
+		
+	}
+	return;
 }
--- src/core/entity.hpp
+++ src/core/entity.hpp
@@ -2,15 +2,34 @@
  * Entity.hpp
  */
 
+#include "D3DX10math.h"
+
+enum Type { ENTITY, SHIP, BASE, ASTEROID};
+
 class Entity {
-private:
-	static id_gen;
+protected:
+	static int id_gen;
 
 public:
-	const long id;
-	Float3 position;
-	Quaternion orientation;
+	// Identification
+	int id;
+	Type type;
+
+	// Physics
+	D3DXVECTOR3 pos;
+	D3DXVECTOR3 dir;
+	float velocity;
+	float maxVelocity;
+	float force;
+	float mass;
+	bool isThrustOn;
 
 	Entity();
-	Entity(position, orientation);
-}
+	Entity(D3DXVECTOR3, D3DXVECTOR3);
+	void init();
+
+	void rotate(D3DXVECTOR3);
+	void toggleThrust();
+	void calculate(float);
+
+};
