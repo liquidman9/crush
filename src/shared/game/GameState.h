49f72d7f68c04046644b87eb9ca3ce825e7a2369
Tyler Hodgkins <ty1824@gmail.com>
2013-04-16 16:52:26 -0700

    Added gamestate

---
+++ src/shared/game/GameState.h
@@ -0,0 +1,51 @@
+#pragma once
+
+// Global includes
+#include <vector>
+
+// Project includes
+#include <shared/game/Entity.h>
+
+//#include"Network.h"
+class GameState
+{
+public:
+
+	GameState(void):m_entities() {
+	};
+
+	GameState(GameState const & g): m_entities(g.getEntities()){
+	};
+
+	vector<Entity*> getEntities() const {
+		return m_entities;
+	};
+
+	void push_back(Entity *e) {
+		m_entities.push_back(e);
+	};
+
+	unsigned int size() {
+		return m_entities.size();
+	}
+
+	Entity*& operator[](unsigned int i) {
+		return m_entities[i];
+	}
+
+	void clear() {
+		for(unsigned int i = 0; i < m_entities.size(); i++){
+			if(m_entities[i])
+				delete m_entities[i];
+		}
+	};
+
+	virtual ~GameState(void) {
+		clear();
+	};
+
+private:
+	vector<Entity*> m_entities;
+
+};
+
