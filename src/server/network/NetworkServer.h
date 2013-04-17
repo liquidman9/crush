0c7455115cf53fd0d06d32f5707bfa9fbaa26ced
Tyler Hodgkins <ty1824@gmail.com>
2013-04-16 17:09:19 -0700

    Added header files

---
+++ src/server/network/NetworkServer.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <vector>
+#include <map>
+
+#include <shared/network/Network.h>
+#include <shared/network/NetworkDecoder.h>
+#include <shared/game/Entity.h>
+
+typedef Entity Event;
+//typedef string Entity;
+typedef vector<Entity *> EventBuff_t;
+typedef GameState State_t;
+
+class NetworkServer :
+	public Network
+{
+public:
+
+	//creates a NetworkServer with default port and any ip address
+	//Throws runtime_error exception if socket cannot be opened
+	NetworkServer(void);
+
+	//creates NetworkServer with ip and port number passed
+	//note: ip should be in the form "127.0.0.1"
+	//Throws runtime_error exception if socket cannot be opened
+	NetworkServer(string ip, unsigned short port);
+
+	//creates NetworkServer with any ip address and passed port
+	//Throws runtime_error exception if socket cannot be opened
+	NetworkServer(unsigned short port);
+
+	//returns true if there are new events available
+	bool eventsAvailable() { return m_eventsAvailable; };
+
+	//returns a vector of events received
+	EventBuff_t getEvents();
+
+	void broadcastGameState(const State_t &);
+
+	
+
+
+	virtual ~NetworkServer(void);
+private:
+
+	void sendToClient(char * const buff, int size, Network &client);
+	WSADATA wsa;
+	SOCKET m_sock;
+	map <Network, Network> m_connectedClients;
+	EventBuff_t m_eventsBuffer;
+	void bindSocket();
+	bool m_eventsAvailable;
+	char m_packetData[MAX_PACKET_SIZE];
+	
+	//thread stuff
+	CRITICAL_SECTION m_cs;
+	HANDLE m_hThread;
+	void updateEventsBuffer();
+
+	static unsigned __stdcall ThreadStaticEntryPoint(void * pThis) {
+		NetworkServer * pthX = (NetworkServer*)pThis;
+		pthX->updateEventsBuffer();	
+		return 1;
+	}
+
+};
+
---
+++ src/shared/network/NetworkDecoder.h
@@ -0,0 +1,60 @@
+#pragma once
+
+// Global includes
+#include <vector>
+
+// Project includes
+#include <shared/game/Entity.h>
+#include <shared/network/Network.h>
+
+typedef Entity Event;
+
+class NetworkDecoder
+{
+private:
+	unsigned int m_size;
+	const char *m_head;
+	//const char *m_curr_head;
+
+public:
+	NetworkDecoder(const char *head, unsigned int size) {
+		char * tmp = new char[size];
+		m_head = tmp;
+		m_size = size;
+		//m_curr_head = m_head;
+		memcpy(tmp, head, size);
+	};
+
+	void decodeGameState(GameState &g) {
+		Entity* ep = NULL;
+		const char* curr_head = m_head;
+		for(unsigned int size = 0; size < m_size; size += ep->size() ){
+			switch(*(ENUM_TYPE*) m_head) {
+			case ENTITY:
+				ep = new Entity();
+				ep->decode(curr_head);
+				g.push_back(ep);
+				break;
+			}
+		}
+	};
+
+	void decodeEvents(vector<Event*> &g) {
+		Event* ep = NULL;
+		const char* curr_head = m_head;
+		for(unsigned int size = 0; size < m_size; size += ep->size() ){
+			/*switch(*(ENUM_TYPE*) m_head) {
+			case ENTITY:*/
+				ep = new Entity();
+				ep->decode(curr_head);
+				g.push_back(ep);
+				break;
+			/*}*/
+		}
+		
+	}
+	virtual ~NetworkDecoder(void) {
+		delete m_head;
+	}
+};
+
