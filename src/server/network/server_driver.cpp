cb9690873b274736b9b4063bb67e4ff92f99455a
mckgamer <mckgamer@yahoo.com>
2013-04-17 15:45:35 -0700

    mkirby: Adding test server driver.

---
+++ src/server/network/server_driver.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<server/network/NetworkServer.h>
+#include <shared/InputState.h>
+
+using namespace std;
+
+void printEntity(Entity const &e) {
+	cout << e.getID() << " " << e.m_pos.x << " " << e.m_pos.y << " " << e.m_pos.z
+		<< e.m_dir.x << " " << e.m_dir.y << " " << e.m_dir.z << endl;
+	}
+
+int main(){
+	try{
+		cout << "Server:" << endl;
+		NetworkServer server(8888);
+		cout << "socket created:" << endl;
+		EventBuff_t eventBuff;
+		State_t gameState;
+		D3DXVECTOR3 m_pos(2,2,2);
+		D3DXVECTOR3 m_dir(1,1,1);
+		//gameState.push_back(Entity());
+		Entity *test = new Ship(m_pos, m_dir, 1, false);
+		gameState.push_back(test);
+		D3DXVECTOR3 m_pos1(1,1,1);
+		D3DXVECTOR3 m_dir1(0,0,1);
+		test = new Ship(m_pos1, m_dir, 2, true);
+		gameState.push_back(test);
+
+		for(;;) {
+			if (server.eventsAvailable()) {
+				eventBuff = server.getEvents();
+				for(unsigned int i = 0; i < eventBuff.size(); i++) {
+					cout << "Event recieved." << endl;
+					//cout << *eventBuff[i] << endl;
+					gameState[1]->m_pos.z += (float)((((InputState *)eventBuff[i])->thrust))/250.0;
+				}
+			}
+			if(eventBuff.size() < 50 && !eventBuff.empty()) {
+				//gameState[1]->m_pos.z += .025;
+				//gameState[0]->m_pos.z += .05;
+				server.broadcastGameState(gameState);
+			}
+			if(!eventBuff.empty()) {
+				eventBuff.clear();
+			}
+			Sleep(16);
+		}
+	} catch (exception &e) {
+		cout << "exception occured!" << endl;
+		cerr << e.what() << endl;
+		system("pause");
+	}
+
+}
