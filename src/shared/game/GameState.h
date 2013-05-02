b5546c157424034abd86fc20c200224e6181b94a
Eric Crawford <liquidman9@gmail.com>
2013-05-02 13:02:45 -0700

    quick and dirty fix to dealing with multiple gamestates

--- src/shared/game/GameState.h
+++ src/shared/game/GameState.h
@@ -64,42 +64,45 @@ public:
 
 private:
 	void decode(const char *head, const unsigned int size) {
-		Entity* ep = NULL;
-		memcpy((char* ) &m_meta, head, sizeof(m_meta));
-		clear();
+		Entity* ep = NULL;		
 		const char* cur_head = head + sizeof(m_meta);
-		for(unsigned int cur_size = 0; cur_size < size - sizeof(m_meta); cur_size += ep->size() ){
-			ep = NULL;
-			Type a = *(Type*) (cur_head + cur_size);
-			switch(a) {
-			case SHIP: 
-				ep = new C_Ship();
-				ep->decode(cur_head + cur_size);
-				this->push_back(ep);
-				break;
-			case ASTEROID:
-				ep = new C_Asteroid();
-				ep->decode(cur_head + cur_size);
-				this->push_back(ep);
-				break;
-			case TRACTORBEAM:
-				ep = new C_TractorBeam();
-				ep->decode(cur_head + cur_size);
-				this->push_back(ep);
-				break;
-			case MOTHERSHIP:
-				ep = new C_Mothership();
-				ep->decode(cur_head + cur_size);
-				this->push_back(ep);
-				break;
-			case RESOURCE:
-				ep = new C_Resource();
-				ep->decode(cur_head + cur_size);
-				this->push_back(ep);
-				break;
-			default:
-				cerr << "ERROR decoding entity state. Unknown type: " << (int) (*(ENUM_TYPE*)( cur_head + cur_size)) << endl;
-				break;
+		for(unsigned int cur_size = 0; cur_size < size; cur_size += m_meta.size){
+			memcpy((char* ) &m_meta, head, sizeof(m_meta));
+			unsigned int size = m_meta.size;
+			clear();
+			for(unsigned int cur_size = 0; cur_size < size - sizeof(m_meta); cur_size += ep->size() ){
+				ep = NULL;
+				Type a = *(Type*) (cur_head + cur_size);
+				switch(a) {
+				case SHIP: 
+					ep = new C_Ship();
+					ep->decode(cur_head + cur_size);
+					this->push_back(ep);
+					break;
+				case ASTEROID:
+					ep = new C_Asteroid();
+					ep->decode(cur_head + cur_size);
+					this->push_back(ep);
+					break;
+				case TRACTORBEAM:
+					ep = new C_TractorBeam();
+					ep->decode(cur_head + cur_size);
+					this->push_back(ep);
+					break;
+				case MOTHERSHIP:
+					ep = new C_Mothership();
+					ep->decode(cur_head + cur_size);
+					this->push_back(ep);
+					break;
+				case RESOURCE:
+					ep = new C_Resource();
+					ep->decode(cur_head + cur_size);
+					this->push_back(ep);
+					break;
+				default:
+					cerr << "ERROR decoding entity state. Unknown type: " << (int) (*(ENUM_TYPE*)( cur_head + cur_size)) << endl;
+					break;
+				}
 			}
 		}
 	}
